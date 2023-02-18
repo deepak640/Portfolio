db5400362906cf24cbb0d6954352a139820e8747
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-02-18 19:14:24 +0530

    enabling active navbar

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-// typing function
-
-var typed = new Typed(".auto-type", {
-  strings: [ "Coder", "Developer", "Student" ],
-  typeSpeed: 100,
-  backSpeed: 50,
-  loop: true,
-});
-
 // loading function
 
 var loader = document.getElementById("loading")
 window.addEventListener("load", function () {
   loader.style.display = "none";
 })
+
+// active tab
+
+const activepage = window.location.pathname
+const navlinks = document.querySelectorAll('nav a').forEach(link => {
+  if (link.href.includes(`${activepage}`)) {
+    const css = window.getComputedStyle(link, '::after')
+    link.style.setProperty('--width','100%')
+
+  }
+}); 
