7c36669a9371f7dc5ddfe8384a6d6cdbf75affde
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-02-19 11:48:51 +0530

    fixing active tab for '/' route

--- js/script.js
+++ js/script.js
@@ -9,9 +9,14 @@ window.addEventListener("load", function () {
 
 const activepage = window.location.pathname
 const navlinks = document.querySelectorAll('nav a').forEach(link => {
-  if (link.href.includes(`${activepage}`)) {
+  if (link.href.includes('index.html') && activepage.includes('Portfolio/')) {
+    link.style.setProperty('--width', '100%')
+    console.log(activepage);
+  }
+  else if (link.href.includes(`${activepage}`) && activepage.includes(link.href)) {
+    console.log('nothing');
     const css = window.getComputedStyle(link, '::after')
-    link.style.setProperty('--width','100%')
+    link.style.setProperty('--width', '100%')
 
   }
 }); 
